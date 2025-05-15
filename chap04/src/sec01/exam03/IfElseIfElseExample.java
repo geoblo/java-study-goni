00898f3a829c34d4856eeb0367eebd481421f87b
geoblo <geoblo.naver.com>
2025-05-15 12:44:31 +0900

    up

--- chap04/src/sec01/exam03/IfElseIfElseExample.java
+++ chap04/src/sec01/exam03/IfElseIfElseExample.java
@@ -1,5 +1,7 @@
 package sec01.exam03;
 
+import java.util.Scanner;
+
 public class IfElseIfElseExample {
 
 	public static void main(String[] args) {
@@ -33,7 +35,21 @@ public class IfElseIfElseExample {
 //		  0  => 숫자
 //		  가  => 기타문자
 		
-		
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.print("문자 하나를 입력하세요: ");
+        char ch = scanner.nextLine().charAt(0); // 문자열 중 첫 번째 문자만 가져옴
+        System.out.println((int) ch);
+        
+        if (ch >= 'A' && ch <= 'Z') {
+        	System.out.println("대문자");
+        } else if (ch >= 'a' && ch <= 'z') {
+        	System.out.println("소문자");
+        } else if (ch >= '0' && ch <= '9') {
+        	System.out.println("숫자");
+        } else {
+        	System.out.println("기타문자");
+        }
 	}
 
 }
