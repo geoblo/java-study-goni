3952edd1ef240d56625f91cb005f0a7b6193dfee
geoblo <geoblo.naver.com>
2025-05-23 11:54:21 +0900

    up

--- chap07/src/sec03/exam03/quiz/Run.java
+++ chap07/src/sec03/exam03/quiz/Run.java
@@ -31,7 +31,13 @@ public class Run {
 //	이비정의 급여: 250
 	
 	public static void main(String[] args) {
-		
+		FormalEmployee fe = new FormalEmployee("홍길동", "총무부", 1, 5000);
+	    System.out.println(fe.getInfo());
+	    System.out.println(fe.getName() + "의 급여: " + fe.getPay());
+	    
+	    InformalEmployee ie = new InformalEmployee("이비정", "영업부", 250);
+	    System.out.println(ie.getInfo());
+	    System.out.println(ie.getName() + "의 급여: " + ie.getPay());
 	}
 	
 }
