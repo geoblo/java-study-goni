179d7c6ee4d9dec276aee1388825b235e22607e8
geoblo <geoblo.naver.com>
2025-05-26 16:11:20 +0900

    ㄴ

--- chap10/src/sec02/exam02/quiz/TryCatchFinallyFlow.java
+++ chap10/src/sec02/exam02/quiz/TryCatchFinallyFlow.java
@@ -16,8 +16,9 @@ public class TryCatchFinallyFlow {
 			System.out.println(5);
 		}
 		System.out.println(6);
+		// 정답: 1 2 3 5 6
 		
-		// 정답: 
+		System.out.println("=====");
 		
 		// 2. 예외가 발생한 경우
 		System.out.println(1);
@@ -25,6 +26,7 @@ public class TryCatchFinallyFlow {
 			System.out.println(2);
 			
 			System.out.println(0/0);
+			// 예외 발생 => 발생한 예외와 일치하는 catch 블럭이 있는지 확인 => 예외 처리됨
 			
 			System.out.println(3);
 		} catch (ArithmeticException e) {
@@ -33,15 +35,17 @@ public class TryCatchFinallyFlow {
 			System.out.println(5);
 		}
 		System.out.println(6);
+		// 정답: 1 2 4 5 6
 		
-		// 정답: 
+		System.out.println("=====");
 		
 		// 3. 예외가 발생한 경우
 		System.out.println(1);
 		try {
 			System.out.println(2);
 			
-			System.out.println(args[0]);
+			System.out.println(args[0]); 
+			// 예외 발생 => 발생한 예외와 일치하는 catch 블럭을 찾지 못하면 예외는 처리되지 못함 => 비정상 종료
 			
 			System.out.println(3);
 		} catch (ArithmeticException e) {
@@ -50,8 +54,7 @@ public class TryCatchFinallyFlow {
 			System.out.println(5);
 		}
 		System.out.println(6);
-		
-		// 정답: 
+		// 정답: 1 2 5 (비정상 종료됨)
 	}
 
 }
