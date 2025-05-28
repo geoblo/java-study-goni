36faa60b859e341ddf8df80b9b0070f18b0b81f8
geoblo <geoblo.naver.com>
2025-05-28 14:09:54 +0900

    up

---
+++ chap11/src/sec01/quiz_string/Quiz1.java
@@ -0,0 +1,21 @@
+package sec01.quiz_string;
+
+public class Quiz1 {
+	// Quiz
+//	입력된 문자열 중 숫자들의 합계를 출력하시오.
+
+//	[입력]
+//	1) 1,2,3,4,5
+//	2) 1-2-3-4-5
+//	3) a1b2c3de45
+
+//	[출력]
+//	1) 15
+//	2) 15
+//	3) 15
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
