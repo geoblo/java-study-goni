d68ff804d7c83d878dd420f959f68910a1fc0f64
geoblo <geoblo.naver.com>
2025-06-05 16:39:01 +0900

    up

--- chap14/src/sec01/exam13/quiz/Quiz1.java
+++ chap14/src/sec01/exam13/quiz/Quiz1.java
@@ -1,5 +1,8 @@
 package sec01.exam13.quiz;
 
+import java.io.FileReader;
+import java.io.Reader;
+
 public class Quiz1 {
 	// Quiz
 //	FileReader를 사용하여 
@@ -10,7 +13,22 @@ public class Quiz1 {
 	public static void main(String[] args) {
 		char[] buffer = new char[100];
 		
-		
+		try (Reader reader = new FileReader("C:/Temp/output.txt")) {
+			int readCharNum;
+			while ((readCharNum = reader.read(buffer)) != -1) {
+				// 읽은 char 배열을 문자열로 변환 후 출력 
+//				String data = new String(buffer, 0, readCharNum);
+//				System.out.println(data);
+				
+				// 또는 읽은 문자 수만큼 반복하면서 배열에 저장된 문자를 하나씩 출력
+				// 문자 하나씩 직접 처리해야 하는 경우
+				for (int i = 0; i < readCharNum; i++) {
+					System.out.print(buffer[i]);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
