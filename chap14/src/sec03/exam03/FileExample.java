93b4ff78f96bb7e0dfe45f0492db4eddee1c7686
geoblo <geoblo.naver.com>
2025-06-09 14:48:07 +0900

    up

--- chap14/src/sec03/exam03/FileExample.java
+++ chap14/src/sec03/exam03/FileExample.java
@@ -2,6 +2,12 @@ package sec03.exam03;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 // File 클래스
 // 파일 및 폴더 생성과 삭제
@@ -47,16 +53,80 @@ public class FileExample {
 		// 폴더 삭제
 		// 폴더가 비어있어야만 삭제가 가능
 		// 폴더 안에 파일이나 하위 폴더가 있다면 먼저 삭제해야 함
-		
-		
+		if (dirTest.delete()) {
+			System.out.println("videos 폴더 삭제 완료");
+		}
 		dirTest = new File("C:/Temp/test");
 		if (dirTest.delete()) {
 			System.out.println("test 폴더 삭제 완료");
 		}
 		
+		// 파일 삭제
+		if (file3.delete()) {
+			System.out.println("file3.txt 파일 삭제 완료");
+		}
 		
+		// 파일 및 폴더 유형 확인
+		// 파일인지 폴더인지 여부를 true/false로 리턴함
+		System.out.println("폴더? " + dir.isDirectory());
+		System.out.println("파일? " + dir.isFile());
+		System.out.println("폴더? " + file1.isDirectory());
+		System.out.println("파일? " + file1.isFile());
 		
+		// 상위(부모) 폴더 확인
+		// 파일이나 폴더가 속한 상위(부모) 폴더의 경로를 문자열로 리턴함
+		System.out.println(dir.getParent());
+		// 상위(부모) 폴더를 File객체로 만든 후 리턴함
+		File parentDir = dir.getParentFile();
 		
+		// 파일 및 폴더의 전체 경로 확인
+		System.out.println(dir.getPath());
+		System.out.println(file1.getPath());
+		
+		// Temp 폴더 정보 출력하기
+		File temp = new File("C:/Temp");
+		
+		if (temp.exists() && temp.isDirectory()) {
+			// Temp 폴더에 있는 파일 및 하위 폴더를 File 배열로 리턴
+			File[] contents = temp.listFiles();
+			
+			System.out.println("시간\t\t\t형태\t\t크기\t이름");
+			System.out.println("--------------------------------------------------------");
+			
+			// 날짜 포맷 지정
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd a HH:mm");
+			
+			// java.time API 사용 시
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd a HH:mm");
+			
+			// 반복문을 사용하여 모든 파일과 폴더 확인 가능
+			for (File file : contents) {
+				// 파일의 마지막 수정 날짜 확인
+				// 마지막 수정 시간을 밀리초 단위로 반환
+				// 날짜 형식으로 바꾸기 위해 SimpleDateFormat 활용
+				System.out.print(sdf.format(new Date(file.lastModified())));
+				
+				// java.time API 사용 시
+				// long -> Instant -> ZonedDateTime(또는 LocalDateTime)
+//				ZonedDateTime dateTime = Instant.ofEpochMilli(file.lastModified()) // 에포크 기준 밀리초 -> Instant로 변환
+//												.atZone(ZoneId.systemDefault()); // 한국이니까 Asia/Seoul(UTC+9)
+//				System.out.println(" " + dateTime.format(formatter));
+				
+				// 폴더인지 검사
+				if (file.isDirectory()) {
+					// 폴더 이름만 출력
+					System.out.print("\t<DIR>\t\t\t" + file.getName());
+				} else {
+					// 파일 크기 + 파일 이름 출력
+					// 파일의 크기를 바이트 단위로 반환
+					// 필요에 따라 단위를 환산하여 사용
+					System.out.print("\t\t\t" + file.length() + "\t" + file.getName());
+				}
+				System.out.println();
+			}
+		} else {
+			System.out.println("폴더가 아니거나 존재하지 않습니다.");
+		}
 	}
 
 }
